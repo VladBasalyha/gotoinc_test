9a9d83f8cfb0debad9ad4f4c8049d2aecdafd4ab
VLADYSLAV <basalyga.180999@gmail.com>
2023-11-14 21:23:57 +0200

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <script setup>
-import FormField from './components/FormField.vue'
-import { ref } from 'vue'
-// const onSubmit = () => {
-//   console.log(request.value.originCity)
-// }
+import Dialog from '../src/components/DialogWindow.vue'
 import { v4 as uuidv4 } from 'uuid'
-// import Dialog from '../src/components/DialogWindow.vue'
-// import FormField from './components/FormField.vue'
+import { ref } from 'vue'
+
 const requests = ref([])
 
-// const showDialog = ref(null)
+const showDialog = ref(null)
 
-// const cancel = () => {
-//   showDialog.value = null
-// }
+const cancel = () => {
+  showDialog.value = null
+}
 
-// async function fetchRequests() {
-//   const existingRequests = JSON.parse(localStorage.getItem('requests')) || []
-//   requests.value = existingRequests
-// }
+async function fetchRequests() {
+  const existingRequests = JSON.parse(localStorage.getItem('requests')) || []
+  requests.value = existingRequests
+}
 
-// fetchRequests()
+fetchRequests()
 
 const request = ref({
   originCity: '',
@@ -30,13 +26,13 @@ const request = ref({
   parcelDescription: ''
 })
 
-// // Delete the request
-// const confirm = () => {
-//   requests.value = requests.value.filter((request) => request.id !== showDialog.value)
+// Delete the request
+const confirm = () => {
+  requests.value = requests.value.filter((request) => request.id !== showDialog.value)
 
-//   showDialog.value = null
-//   localStorage.setItem('requests', JSON.stringify(requests.value))
-// }
+  showDialog.value = null
+  localStorage.setItem('requests', JSON.stringify(requests.value))
+}
 
 function onSubmit() {
   const requestData = {
@@ -49,6 +45,16 @@ function onSubmit() {
   }
 
   // Push the new request to the requests ref
+  if (
+    !requestData.originCity ||
+    !requestData.destinationCity ||
+    !requestData.parcelType ||
+    !requestData.dispatchDate ||
+    !requestData.parcelDescription
+  ) {
+    alert('All fields are required')
+    return
+  }
   requests.value.push(requestData)
 
   // Save the updated requests array to local storage
@@ -61,9 +67,46 @@ function onSubmit() {
 </script>
 
 <template>
-  <FormField @submit="onSubmit"></FormField>
-  <!-- <FormField @submit="onSubmit" />
   <div>
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="origin-city">From:</label>
+        <input type="text" class="form-control" id="origin-city" v-model="request.originCity" />
+      </div>
+      <div class="form-group">
+        <label for="destination-city">To:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="destination-city"
+          v-model="request.destinationCity"
+        />
+      </div>
+      <div class="form-group">
+        <label for="parcel-type">Type of parcel:</label>
+        <select class="form-control" id="parcel-type" v-model="request.parcelType">
+          <option value="gadgets">Gadgets</option>
+          <option value="drinks">Drinks</option>
+          <option value="clothes">Clothes</option>
+          <option value="medicines">Medicines</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="dispatch-date">Date of dispatch:</label>
+        <input type="date" class="form-control" id="dispatch-date" v-model="request.dispatchDate" />
+      </div>
+      <div class="form-group">
+        <label for="parcel-description">Parcel description:</label>
+        <textarea
+          class="form-control"
+          id="parcel-description"
+          rows="3"
+          v-model="request.parcelDescription"
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
     <h1>All requests</h1>
     <ul>
       <li v-for="request in requests" :key="request.id">
@@ -82,15 +125,13 @@ function onSubmit() {
                 description="Are you sure you want to delete this task?"
               />
             </Teleport>
-
-
-  </li>
+          </li>
           <li>To:{{ request.destinationCity }}</li>
           <li>Type:({{ request.parcelType }})</li>
           <li>Date:{{ request.dispatchDate }}</li>
           <li>{{ request.parcelDescription }}</li>
         </ul>
       </li>
-    </ul> -->
-  <!-- </div>  -->
+    </ul>
+  </div>
 </template>
